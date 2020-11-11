2190200880e686f4ab72e3a12dfddae6d85e5986
Daniel Rivas <daniel.rivas@bsc.es>
2020-11-11 18:44:59 +0100

    New endpoint to offload a whole video at once.

--- api/process_client.py
+++ api/process_client.py
@@ -12,11 +12,40 @@ import numpy as np
 import pandas as pd
 import requests
 
-from ..utils.detector import label_map
+sys.path.append('../')
+from utils.detector import label_map, draw_predictions
 
-url = 'http://localhost:{}/infer'
+url = 'http://localhost:{}/{}'
 with open('imagenet.txt', 'r') as f:
     imagenet = json.load(f)
+
+
+def draw_bboxes(img, preds, max_boxes=5, min_score=.5, color=(255, 0, 0)):
+    boxes_drawn = 0
+    
+    img_ = img.copy()
+    for idx, bb in enumerate(preds['boxes']):
+        if boxes_drawn >= max_boxes:
+            break
+
+        if preds['scores'][idx] >= min_score:
+            ymin, xmin, ymax, xmax = bb
+            (left, right, top, bottom) = (
+                xmin * img_.shape[1], # left
+                xmax * img_.shape[1], # top
+                ymin * img_.shape[0], # right
+                ymax * img_.shape[0]  # bottom
+            )
+            cv2.rectangle(img_, (int(left), int(top)),
+                          (int(right), int(bottom)),
+                          color, 1)
+            obj_class = label_map[str(preds['idxs'][idx])]['name']
+            if top - 10 < 0:
+                top = top + 20
+            cv2.putText(img_, str(obj_class), (int(left), int(top)-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+    return img_
 
 
 def process_response(response, detection=False):
@@ -25,13 +54,13 @@ def process_response(response, detection=False):
     preds = json.loads(response.text)['data']
     if detection:
         boxes = preds['boxes']
-        scores = [conf*100 for conf in preds['scores']]
+        scores = [conf for conf in preds['scores']]
         idxs = preds['idxs']
 
-        top5 = [idxs, scores]
+        top5 = [idxs, scores, boxes]
         top5_str = []
         for idx, conf in zip(idxs, scores):
-            top5_str.append(f'{label_map[str(idx)]} ({conf:.2f}%)')
+            top5_str.append(f'{label_map[str(idx)]["name"]} ({conf*100:.2f}%)')
 
         return top5, top5_str
 
@@ -52,6 +81,165 @@ def process_response(response, detection=False):
         top5 = [classes, confs]
 
 
+def process_video(filename, url, framework='torch', no_show=False):
+    detection = True if framework == 'tf' else False
+
+    ts = filename.stem
+
+    date = '-'.join(str(ts).split('-')[:2])
+    hour = str(ts).split('-')[3]
+    minute = str(ts).split('-')[4]
+
+    cap = cv2.VideoCapture(str(filename))
+    frame_id = 0
+    ret, frame = cap.read()
+    data = []
+    
+    error = False 
+    while ret:
+        frame = cv2.resize(frame, (800, 600))
+        _, buf = cv2.imencode('.png', frame)
+        png64 = base64.b64encode(buf)
+
+        top5 = {}
+        top5_str = {}
+        for model in ['edge', 'ref']:
+            try:
+                r = requests.post(url, data={
+                    'model': model,
+                    'img': png64,
+                    'device': 'cuda',
+                    'framework': framework
+                })
+            except ConnectionResetError:
+                error = True
+                break
+
+            preds_top5, preds_str = process_response(r, detection)
+            top5[model] = preds_top5
+            top5_str[model] = preds_str
+            
+        if error:
+            break
+
+        row = [
+            ts,
+            date,
+            hour,
+            minute,
+            frame_id,
+            top5['ref'][0],
+            top5['ref'][1],
+            top5['edge'][0],
+            top5['edge'][1]
+        ]
+        data.append(row)
+
+        if not no_show:
+            cv2.putText(frame, 'Reference Model:', (10, 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            for j, det in enumerate(top5_str['ref']):
+                cv2.putText(frame, det, (10, 20 + 15* (j+1)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            
+            cv2.putText(frame, 'Edge Model:', (10, 500),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            for j, det in enumerate(top5_str['edge']):
+                cv2.putText(frame, det, (10, 500 + 15 * (j+1)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+            if detection:
+                predictions = {
+                    'ref': {
+                        'idxs': top5['ref'][0],
+                        'scores': top5['ref'][1],
+                        'boxes': top5['ref'][2]
+                    },
+                    'edge': {
+                        'idxs': top5['edge'][0],
+                        'scores': top5['edge'][1],
+                        'boxes': top5['edge'][2]
+                    }
+                }
+
+                frame = draw_bboxes(frame, predictions['ref'], max_boxes=5)
+                frame = draw_bboxes(frame, predictions['edge'],
+                                    max_boxes=5, color=(0, 128, 128))
+            cv2.imshow('Detections', frame)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                error = True
+                break
+                sys.exit()
+            elif key == ord("n"):
+                break
+
+        ret, frame = cap.read()
+        frame_id += 1
+
+    if error:
+        return False, data
+
+    return True, data
+
+
+def offload_video(filename, url, model='edge', framework='torch'):
+    print('send video')
+    try:
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        
+        with open(str(filename), 'rb') as video:
+            r = requests.post(url,
+                              files={'video': video},
+                              data={
+                                  'model': model,
+                                  'device': 'cuda',
+                                  'framework': framework
+                              })# , headers=headers)
+        print(r.text)
+    except ConnectionResetError:
+        return False, None
+    except Exception:
+        raise
+
+    ts = filename.stem
+
+    date = '-'.join(str(ts).split('-')[:2])
+    hour = str(ts).split('-')[3]
+    minute = str(ts).split('-')[4]
+
+    preds = json.loads(r.text)['data']
+    print(preds)
+    sys.exit()
+
+    return True, preds
+
+def process_dataset(path, url, 
+                    framework='torch', 
+                    send_video=False, 
+                    no_show=False):
+
+    all_data = []
+    for f in path.glob('*.mkv'):
+        if not send_video:
+            ret, data = process_video(f, url, framework, no_show)
+
+            all_data += data
+
+            if not ret:
+                break
+        else:
+            ret, data = offload_video(f, url, framework=framework)
+
+    columns = ['timestamp', 'date', 'hour', 'minute', 'frame_id',
+               'top5_ref_classes', 'top5_ref_conf',
+               'top5_edge_classes', 'top5_edge_conf']
+
+    detections = pd.DataFrame(all_data, columns=columns)
+    detections.to_csv('detections.csv', sep=',', float_format='.2f', index=False)
+
+
 def main():
     global url
     args = argparse.ArgumentParser()
@@ -68,96 +256,23 @@ def main():
                       default='torch',
                       choices=['torch', 'tf'],
                       help="Framework to use")
+    
+    args.add_argument("--send-video",
+                      default=False,
+                      action="store_true",
+                      help="Send whole video instead of frame by frame")
+    
+    args.add_argument("--no-show",
+                      default=False,
+                      action="store_true",
+                      help="Don't show results in a window.")
 
     config = args.parse_args()
-    url = url.format(config.port)
+    url = url.format(config.port, 'video' if config.send_video else 'infer')
 
     path = Path(config.input)
-
-    data = []
-    error = False
-    for f in path.glob('*.mkv'):
-        ts = f.stem
-
-        date = '-'.join(str(ts).split('-')[:2])
-        hour = str(ts).split('-')[3]
-        minute = str(ts).split('-')[4]
-        # print(f'day: {date} @ {hour}:{minute}')
-
-        cap = cv2.VideoCapture(str(f))
-        frame_id = 0
-        ret, frame = cap.read()
-        while ret:
-            frame = cv2.resize(frame, (800, 600))
-            _, buf = cv2.imencode('.png', frame)
-            png64 = base64.b64encode(buf)
-
-            top5 = {}
-            top5_str = {}
-            for model in ['edge', 'ref']:
-                try:
-                    r = requests.post(url, data={
-                        'model': model,
-                        'img': png64,
-                        'device': 'cuda',
-                        'framework': config.framework
-                    })
-                except ConnectionResetError:
-                    error = True
-                    break
-
-                detection = True if config.framework == 'tf' else False
-                preds_top5, preds_str = process_response(r, detection)
-                top5[model] = preds_top5
-                top5_str[model] = preds_str
-                
-            if error:
-                break
-
-            row = [
-                ts,
-                date,
-                hour,
-                minute,
-                frame_id,
-                top5['ref'][0],
-                top5['ref'][1],
-                top5['edge'][0],
-                top5['edge'][1]
-            ]
-            data.append(row)
-    
-            cv2.putText(frame, 'Reference Model:', (10, 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            for j, det in enumerate(top5_str['ref']):
-                cv2.putText(frame, det, (10, 20 + 15* (j+1)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            
-            cv2.putText(frame, 'Edge Model:', (10, 500),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            for j, det in enumerate(top5_str['edge']):
-                cv2.putText(frame, det, (10, 500 + 15 * (j+1)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            cv2.imshow('Detections', frame)
-
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord("q"):
-                sys.exit()
-            elif key == ord("n"):
-                break
-
-            ret, frame = cap.read()
-            frame_id += 1
-
-        if error:
-            break
-
-    columns = ['timestamp', 'date', 'hour', 'minute', 'frame_id',
-               'top5_ref_classes', 'top5_ref_conf',
-               'top5_edge_classes', 'top5_edge_conf']
-
-    detections = pd.DataFrame(data, columns=columns)
-    detections.to_csv('detections.csv', sep=',', float_format='.2f', index=False)
+    process_dataset(path, url, config.framework,
+                    config.send_video, config.no_show)
 
 if __name__ == '__main__':
     main()
--- api/server.py
+++ api/server.py
@@ -4,9 +4,10 @@ import argparse
 import base64
 import json
 import sys
+import tempfile
 import time
 
-from flask import Flask, request, Response
+from flask import Flask, Response
 from flask_restful import Resource, Api, reqparse
 import cv2
 import numpy as np
@@ -14,16 +15,159 @@ import pandas as pd
 import torch
 from torchvision.models import mobilenet_v2, resnet152
 from torchvision.models.detection import faster_rcnn
+from werkzeug.datastructures import FileStorage
 
-from ..utils.detector import init_detector, run_detector, detect_and_draw, label_map
+sys.path.append('..')
+from utils.detector import init_detector, run_detector
+from utils.detector import ALL_MODELS as TF_MODELS
 
 app = Flask(__name__)
 api = Api(app)
 
 models = {}
+framework = 'torch'
+
 classes = None
 with open('imagenet.txt', 'r') as c:
     classes = json.load(c)
+
+def infer_tf(model, img, device='cpu'):
+    results = run_detector(model, img, model.input_size) 
+    return results
+
+
+@torch.no_grad()
+def infer_torch(model, img, device='cpu'):
+    dev = torch.device('cpu') if device == 'cpu' else device
+    model.to(dev)
+
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = np.array(img)
+    img = img.astype("single") / float(255)
+    x = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0)
+    x = x.to(dev)
+
+    ts0 = time.time()
+    predictions = model(x)
+    del x
+    ts1 = time.time()
+    print(f'inference took {ts1-ts0:.2f} seconds.')
+
+    return predictions[0].detach().cpu().numpy()
+
+
+def get_top_torch(preds, topn=10):
+    if topn > len(preds):
+        topn = len(preds)
+    idxs = np.argpartition(-preds, 5)[:topn]
+    results = []
+    for i, idx in enumerate(idxs):
+        results.append([str(idx), f'{preds[idx]:.2f}'])
+
+    top = {
+        'idxs': results[0],
+        'scores': results[1]
+    }
+    return top
+
+
+def get_top_tf(preds, topn=10):
+    boxes = preds['detection_boxes'][0]
+    scores = preds['detection_scores'][0]
+    class_ids = preds['detection_classes'][0]
+
+    top = {
+        'boxes': boxes[:topn].tolist(),
+        'scores': scores[:topn].tolist(),
+        'idxs': class_ids[:topn].astype(int).tolist()
+    }
+    return top
+
+
+
+class Models(Resource):
+    def get(self):
+        tf_models = [['tf', m] for m in TF_MODELS.keys()]
+        data = pd.DataFrame(tf_models, columns=['framework', 'model'])
+        data = data.to_dict()
+        return {'data': data}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('model', required=True)
+        args = parser.parse_args()
+
+        if args.model not in [m for m in TF_MODELS.keys()]:
+            return {
+                'message': 'Invalid model.'
+            }, 401
+
+        if args.model not in [m for m in models.keys()]:
+            models[args.model] = init_detector(args.model)
+        return {
+            'message': 'Model loaded successfully.'
+        }, 200
+
+
+class Video(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('model', required=True)
+        parser.add_argument('device', required=True)
+        parser.add_argument('framework', required=True)
+        parser.add_argument('video', type=FileStorage, location='files')
+
+ 
+        args = parser.parse_args()
+
+        if args.model not in ['edge', 'ref']:
+            return {
+                'message': 'Invalid model.'
+            }, 401
+        
+        if args.device not in ['cpu', 'cuda']:
+            return {
+                'message': 'Invalid device.'
+            }, 401
+
+        if args.framework not in ['torch', 'tf']:
+            return {
+                'message': 'Invalid framework.'
+            }, 401
+
+        # video = base64.b64decode(args.file)
+        # nparr = np.frombuffer(video, np.uint8)
+        # img = cv2.imdecode(nparr, flags=1)
+        tf = tempfile.NamedTemporaryFile(delete=True)
+        args.video.save(tf)
+
+        cap = cv2.VideoCapture(tf.name)
+        ret, frame = cap.read()
+        frame_id = 0
+        m = models[args.model]
+
+        data = []
+        while ret:
+            if args.framework == 'torch':
+                device = args.device if args.device == 'cpu' else devices[args.model]
+                preds = infer_torch(m, frame, device)
+                data.append(get_top_torch(preds))
+
+            else:
+                preds = infer_tf(m, frame, args.device)
+                data.append(get_top_tf(preds))
+                
+            ret, frame = cap.read()
+
+        return Response(
+            response=json.dumps({
+                "data": data
+            }),
+            status=200,
+            mimetype='application/json'
+        )
+
 
 class Infer(Resource):
     def get(self):
@@ -68,77 +212,28 @@ class Infer(Resource):
         
         if args.framework == 'torch':
             device = args.device if args.device == 'cpu' else devices[args.model]
-            preds = self.infer_torch(m, img, device)
+            preds = infer_torch(m, img, device)
             return Response(
                 response=json.dumps({
-                    "data": self.get_top5_torch(preds)
+                    "data": get_top_torch(preds)
                 }),
                 status=200,
                 mimetype='application/json'
             )
         else:
-            preds = self.infer_tf(m, img, args.device)
+            preds = infer_tf(m, img, args.device)
             return Response(
                 response=json.dumps({
-                    "data": self.get_top5_tf(preds)
+                    "data": get_top_tf(preds)
                 }),
                 status=200,
                 mimetype='application/json'
             )
 
 
-    def infer_tf(self, model, img, device='cpu'):
-        results = run_detector(model, img, model.input_size) 
-        return results
-
-
-    @torch.no_grad()
-    def infer_torch(self, model, img, device='cpu'):
-        dev = torch.device('cpu') if device == 'cpu' else device
-        print(dev)
-        print(type(model))
-        model.to(dev)
-
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = np.array(img)
-        img = img.astype("single") / float(255)
-        x = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0)
-        x = x.to(dev)
-
-        ts0 = time.time()
-        predictions = model(x)
-        del x
-        ts1 = time.time()
-        print(f'inference took {ts1-ts0:.2f} seconds.')
-
-        return predictions[0].detach().cpu().numpy()
-
-
-    def get_top5_torch(self, preds):
-        idxs = np.argpartition(-preds, 5)[:5]
-        results = []
-        for i, idx in enumerate(idxs):
-            results.append([str(idx), f'{preds[idx]:.2f}'])
-        return results
-
-
-    def get_top5_tf(self, preds):
-        boxes = preds['detection_boxes'][0]
-        scores = preds['detection_scores'][0]
-        class_ids = preds['detection_classes'][0]
-
-        top5 = {
-            'boxes': boxes[:5].tolist(),
-            'scores': scores[:5].tolist(),
-            'idxs': class_ids[:5].astype(int).tolist()
-        }
-        print(top5)
-        return top5
-
-
-
-
 api.add_resource(Infer, '/infer')
+api.add_resource(Models, '/models')
+api.add_resource(Video, '/video')
 
 
 def main():
@@ -146,6 +241,7 @@ def main():
     global devices
     global app
     global api
+    global framework
 
     args = argparse.ArgumentParser()
     args.add_argument("-f", "--framework", 
@@ -153,7 +249,13 @@ def main():
                       choices=['torch', 'tf'],
                       help="Framework to use")
 
+    args.add_argument("-p", "--port",
+                      default=5000,
+                      type=int,
+                      help="Port to listen to.")
+
     config = args.parse_args()
+    framework = config.framework
     
     if config.framework == 'torch':
         models['edge'] = mobilenet_v2(pretrained=True)
@@ -174,13 +276,13 @@ def main():
         ref_model = 'Faster R-CNN Inception ResNet V2 1024x1024'
         models['edge'] = init_detector()
         # models['ref'] = init_detector()
-        models['ref'] = init_detector(ref_model)
+        # models['ref'] = init_detector(ref_model)
         # print(models['ref'])
         models['edge'].input_size = (320, 320)
         # models['ref'].input_size = (320, 320)
-        models['ref'].input_size = (1024, 1024)
+        # models['ref'].input_size = (1024, 1024)
 
-    app.run(port=5001)
+    app.run(port=config.port)
     
 
 if __name__ == '__main__':
