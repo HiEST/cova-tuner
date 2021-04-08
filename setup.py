f0775f55253ff60ec566f0e16996e7194b68baf1
Daniel Rivas <daniel.rivas@bsc.es>
2021-04-08 13:08:33 +0200

    +Packaging for PyPI (some files were left out)

--- setup.py
+++ setup.py
@@ -1,36 +1,8 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Setup module."""
+"""Setup module for :mod:`edge_autotune`."""
 
-from setuptools import setup
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
+import setuptools
 
 if __name__ == '__main__':
-    setup(
-        name='edge_autotune',
-        version='0.0.1-dev',
-        license='Apache-2.0',
-        author='Daniel Rivas',
-        author_email='daniel.rivas@bsc.es',
-        url='https://github.com/HiEST/edgeautotuner',
-        py_modules=[
-            'edge_autotune',
-            'edge_autotune.apps',
-            'edge_autotune.utils',
-            'edge_autotune.dnn',
-        ],
-        package_dir={'': 'src'},
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
-            "License :: OSI Approved :: Apache Software License",
-            "Operating System :: Unix"
-        ],
-        description='Edge AutoTune. Framework for automated fine-tuning of edge models',
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-  )
+    setuptools.setup()
