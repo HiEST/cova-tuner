b2471121eb2c7737c12a6c8181b824ee36b24f45
Daniel Rivas <daniel.rivas@bsc.es>
2021-11-15 17:24:11 +0100

    training works on AWS SageMaker and generates model artifacts

--- aws/source_dir/run_training.sh
+++ aws/source_dir/run_training.sh
@@ -17,18 +17,48 @@ ls ${MODEL_DIR}
 env
 
 echo "pdw: `pwd`"
-echo "python train.py \
-    --pipeline_config_path ${PIPELINE_CONFIG_PATH} \
-    --model_dir ${MODEL_DIR} \
-    --num_train_steps ${NUM_TRAIN_STEPS} \
-    --sample_1_of_n_eval_examples ${SAMPLE_1_OF_N_EVAL_EXAMPLES} \
-    --checkpoint-dir ${SM_HP_CHECKPOINT_DIR}"
+echo `ls -lahtr`
+echo `find`
+echo `ls -lahtr /opt/ml/input`
+echo `find /opt/ml/input`
+echo `ls -lahtr ${MODEL_DIR}`
+echo `ls /opt/ml/input/data/train/validation.records`
+# echo "python train.py \
+#     --pipeline_config_path ${PIPELINE_CONFIG_PATH} \
+#     --model_dir ${MODEL_DIR} \
+#     --num_train_steps ${NUM_TRAIN_STEPS} \
+#     --sample_1_of_n_eval_examples ${SAMPLE_1_OF_N_EVAL_EXAMPLES} \
+#     --checkpoint-dir ${SM_HP_CHECKPOINT_DIR}"
 
-python train.py \
+# python train.py \
+#     --pipeline_config_path ${PIPELINE_CONFIG_PATH} \
+#     --model_dir ${MODEL_DIR} \
+#     --num_train_steps ${NUM_TRAIN_STEPS} \
+#     --sample_1_of_n_eval_examples ${SAMPLE_1_OF_N_EVAL_EXAMPLES} \
+#     --checkpoint-dir ${SM_HP_CHECKPOINT_DIR}
+
+echo "===TRAINING THE MODEL=="
+python model_main_tf2.py \
     --pipeline_config_path ${PIPELINE_CONFIG_PATH} \
     --model_dir ${MODEL_DIR} \
     --num_train_steps ${NUM_TRAIN_STEPS} \
+    --num_workers ${NUM_WORKERS} \
     --sample_1_of_n_eval_examples ${SAMPLE_1_OF_N_EVAL_EXAMPLES} \
-    --checkpoint-dir ${SM_HP_CHECKPOINT_DIR}
+    --alsologtostderr
+
+echo "==EVALUATING THE MODEL=="
+python model_main_tf2.py \
+    --pipeline_config_path ${PIPELINE_CONFIG_PATH} \
+    --model_dir ${MODEL_DIR} \
+    --checkpoint_dir ${MODEL_DIR} \
+    --eval_timeout 10
+
+echo "==EXPORTING THE MODEL=="
+python exporter_main_v2.py \
+    --trained_checkpoint_dir ${MODEL_DIR} \
+    --pipeline_config_path ${PIPELINE_CONFIG_PATH} \
+    --output_directory /tmp/exported
+    
+mv /tmp/exported/saved_model /opt/ml/model/1
 
 echo "Finished"
